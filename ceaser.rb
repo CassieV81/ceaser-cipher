18390ad17709ecb2b0543d5c004faa404c4c5cbf
CassieV <obicassievirtue@gmail.com>
2022-07-01 15:33:55 +0200

    Change conditionals

--- ceaser.rb
+++ ceaser.rb
@@ -2,15 +2,19 @@ def ceaser_cipher(string, shift)
   string_array = string.split("")
   string_array.map! do |char|
     p char_byte = char.bytes
-    character = (char_byte[0] + (shift))
+    p character = char_byte[0]
     if character >= 65 && character <= 90 || character >= 97 && character <= 122 
+      p character = character + shift
       p character.chr
-    elsif character < 65 && character > 90 || character < 97 && character > 122
-      p character.chr
+      if character > 90 && character < 97 || character > 122
+        p (character - 26).chr
+      else
+        p character.chr
+      end
     else
-      p character = char_byte[0].chr
+      p character.chr
     end
   end
   p string_array.join
 end
-ceaser_cipher("What a string!", 5)
+ceaser_cipher("What a string123!", 20)
