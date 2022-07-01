01c9d32e25a539ec49fb1d4ddd39e6650295ebea
CassieV <obicassievirtue@gmail.com>
2022-07-01 14:21:43 +0200

    Use if statements to return shifts

--- ceaser.rb
+++ ceaser.rb
@@ -1,7 +1,16 @@
 def ceaser_cipher(string, shift)
-  characters = ('a'..'z').to_a.reverse
-  p characters
   string_array = string.split("")
-  p string_array
+  string_array.map! do |char|
+    p char_byte = char.bytes
+    character = (char_byte[0] + (shift))
+    if character >= 65 && character <= 90 || character >= 97 && character <= 122 
+      p character.chr
+    elsif character < 65 && character > 90 || character < 97 && character > 122
+      p character.chr
+    else
+      p character = char_byte[0].chr
+    end
+  end
+  p string_array.join
 end
-ceaser_cipher('hi', 1)
+ceaser_cipher("What a string!", 5)
