19aedea46fe27be138445a886a56148a5039e883
CassieV <obicassievirtue@gmail.com>
2022-07-13 13:16:40 +0200

    Remove unnecessary print functions

--- ceaser.rb
+++ ceaser.rb
@@ -1,20 +1,20 @@
 def ceaser_cipher(string, shift)
   string_array = string.split("")
   string_array.map! do |char|
-    p char_byte = char.bytes
-    p character = char_byte[0]
+    char_byte = char.bytes
+    character = char_byte[0]
     if character >= 65 && character <= 90 || character >= 97 && character <= 122 
-      p character = character + shift
-      p character.chr
+      character = character + shift
+      character.chr
       if character > 90 && character < 97 || character > 122
-        p (character - 26).chr
+        (character - 26).chr
       else
-        p character.chr
+        character.chr
       end
     else
-      p character.chr
+      character.chr
     end
   end
-  p string_array.join
+  puts string_array.join
 end
-ceaser_cipher("What a string123!", 20)
+ceaser_cipher("My name is Cassie!", 10)
